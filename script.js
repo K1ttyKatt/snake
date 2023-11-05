2beddfd7594b54ed93185fbb4558ff16f2714b20
K1ttyKatt <148871710+K1ttyKatt@users.noreply.github.com>
2023-11-05 18:59:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const boxSize = 20;
     const canvasSize = canvas.width / boxSize;
+    let snake = [{x: 10, y: 10}];
+    let food = {x: Math.floor(Math.random() * canvasSize), y: Math.floor(Math.random() * canvasSize)};
+    let direction = 'right';
+    let score = 0;
+    let timer = 0;
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
 
