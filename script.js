2cc24bd13c5a7306bd722d263199eb5199e5c8a7
K1ttyKatt <148871710+K1ttyKatt@users.noreply.github.com>
2023-11-05 19:07:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const letters = '0123456789ABCDEF';
         let color = '#';
         for (let i = 0; i < 6; i++) {
+               color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
 
