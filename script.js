50fb9d43ba2b70b30a72ca8832dd114bbc1ec5bd
K1ttyKatt <148871710+K1ttyKatt@users.noreply.github.com>
2023-11-05 19:30:48 -0300

    script.js

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < 6; i++) {
                color += letters[Math.floor(Math.random() * 16)];
         }
+        
         return color;
     }
 
