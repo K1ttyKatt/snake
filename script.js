477987882d5518346e12a212eb210a5a47afee2e
K1ttyKatt <148871710+K1ttyKatt@users.noreply.github.com>
2023-11-05 18:55:26 -0300

    Script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
+
+    const boxSize = 20;
+    const canvasSize = canvas.width / boxSize;
 
